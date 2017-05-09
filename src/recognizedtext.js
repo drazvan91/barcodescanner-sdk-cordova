18a0adc9364cf42537302d65c23edcc3fa0dd22e
Moritz Hartmeier <moritz@scandit.com>
2017-05-09 09:33:25 +0200

    Fixed recognized text.
    (cherry picked from commit 95b2a0deb1029b49fcafeeed86090565a62e6b69)

--- src/recognizedtext.js
+++ src/recognizedtext.js
@@ -1,10 +1,6 @@
-cordova.define("com.mirasense.scanditsdk.plugin.RecognizedText", function(require, exports, module) {
-
 function RecognizedText(text) {
 	this.text = text;
 	this.rejected = false;
 }
 
 module.exports = RecognizedText;
-
-});

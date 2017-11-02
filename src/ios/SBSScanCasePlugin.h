c3c63a071438faec5b6f743f43d232d600b7c919
Luca Torella <luca@scandit.com>
2017-11-02 17:57:49 +0100

    Add volumeButtonToScanEnabled method to ScanCase
    (cherry picked from commit 1350eb8a91c2772a92111a45e527111464092117)

--- src/ios/SBSScanCasePlugin.h
+++ src/ios/SBSScanCasePlugin.h
@@ -9,16 +9,15 @@
 //  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 //  express or implied. See the License for the specific language governing permissions and
 //  limitations under the License.
+
 #import <Foundation/Foundation.h>
 #import "Cordova/CDVPlugin.h"
 
-
 @interface SBSScanCasePlugin : CDVPlugin
 
-
 - (void)acquire:(CDVInvokedUrlCommand *)command;
+- (void)setState:(CDVInvokedUrlCommand *)command;
+- (void)finishDidScanCallback:(CDVInvokedUrlCommand *)command;
+- (void)volumeButtonToScanEnabled:(CDVInvokedUrlCommand *)command;
 
-- (void)setState:(CDVInvokedUrlCommand*)command;
-
-- (void)finishDidScanCallback:(CDVInvokedUrlCommand*)command;
 @end

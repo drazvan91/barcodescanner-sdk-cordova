98a57504abce719caaeab0de7cc920a67e1c9a13
Robert Nawrot <robert@scandit.com>
2018-01-17 09:10:51 +0100

    Added a function to pass a dictionary for any tracked code which is passed to the native layer.

--- src/matrixScanSession.js
+++ src/matrixScanSession.js
@@ -2,10 +2,15 @@
 function MatrixScanSession(newlyTrackedCodes) {
 	this.newlyTrackedCodes = newlyTrackedCodes;
 	this.rejectedTrackedCodes = [];
+	this.trackedCodeStates = {};
 }
 
 MatrixScanSession.prototype.rejectTrackedCode = function(code) {
 	this.rejectedTrackedCodes.push(code.uniqueId);
+}
+
+MatrixScanSession.prototype.setStateForTrackedCode = function(code, stateObject) {
+	this.trackedCodeStates[code.uniqueId] = stateObject;
 }
 
 module.exports = MatrixScanSession;

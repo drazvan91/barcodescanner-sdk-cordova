52297a7394176d615cbcad72c5d617234dbc9fc1
Petra Donka <petra@scandit.com>
2018-03-15 10:28:45 +0100

    added documentation and fixed module to only have the Scandit namespace

--- types/test.ts
+++ types/test.ts
@@ -1,15 +1,15 @@
 /// <reference path="index.d.ts"/>
 
-import { Barcode, Callbacks, Constraints, License, MatrixScanSession, Point, Quadrilateral, ScanSession, TextRecognitionSettings, Rect, SymbologySettings, BarcodePicker, Margins, ScanSettings, ScanOverlay, ScanCase, RecognizedText } from "scandit";
+import { Scandit } from "barcodescanner-sdk-cordova";
 
-const barcode = new Barcode();
+const barcode = new Scandit.Barcode();
 barcode.isGs1DataCarrier();
 barcode.isRecognized();
 console.log(barcode.data, barcode.rawData, barcode.symbology, barcode.compositeFlag);
 
-const symbology = Barcode.Symbology.CODE39;
+const symbology = Scandit.Barcode.Symbology.CODE39;
 
-const callbacks = new Callbacks();
+const callbacks = new Scandit.Callbacks();
 callbacks.didScan = console.log;
 callbacks.didManualSearch = console.log;
 callbacks.didCancel = console.log;
@@ -18,56 +18,56 @@ callbacks.didRecognizeNewCodes = console.log;
 callbacks.didChangeState = console.log;
 callbacks.didChangeProperty = console.log;
 
-const callbacks2: Callbacks = {
+const callbacks2: Scandit.Callbacks = {
     didScan: console.log,
 };
 
-let constraints = new Constraints();
+let constraints = new Scandit.Constraints();
 constraints.topMargin = 12;
 constraints.width = 1;
 constraints.height = 0.3;
 
-License.setAppKey("foobarkey");
+Scandit.License.setAppKey("foobarkey");
 
-const margins = new Margins(10, 10, 10, 10);
-const marginsPercentage = new Margins('10%', '10%', '10%', '10%');
+const margins = new Scandit.Margins(10, 10, 10, 10);
+const marginsPercentage = new Scandit.Margins('10%', '10%', '10%', '10%');
 
-const matrixScanSession = new MatrixScanSession([barcode]);
+const matrixScanSession = new Scandit.MatrixScanSession([barcode]);
 matrixScanSession.rejectTrackedCode(matrixScanSession.newlyTrackedCodes[0]);
 
-const point = new Point(10, 10);
+const point = new Scandit.Point(10, 10);
 console.log(point.x, point.y);
 
-const quad = new Quadrilateral(point, point, point, point);
+const quad = new Scandit.Quadrilateral(point, point, point, point);
 console.log(quad.bottomLeft, quad.bottomRight, quad.topLeft, quad.topRight);
 
-const bp = new BarcodePicker();
-const scanSession = new ScanSession([barcode], [barcode], [barcode], bp);
+const bp = new Scandit.BarcodePicker();
+const scanSession = new Scandit.ScanSession([barcode], [barcode], [barcode], bp);
 scanSession.pauseScanning();
 scanSession.stopScanning();
 scanSession.rejectCode(scanSession.allRecognizedCodes[0]);
 scanSession.rejectCode(scanSession.newlyLocalizedCodes[0]);
 scanSession.rejectCode(scanSession.newlyRecognizedCodes[0]);
 
-const textRecognitionSettings = new TextRecognitionSettings();
-textRecognitionSettings.areaPortrait = new Rect(1, 1, 1, 1);
+const textRecognitionSettings = new Scandit.TextRecognitionSettings();
+textRecognitionSettings.areaPortrait = new Scandit.Rect(1, 1, 1, 1);
 textRecognitionSettings.regex = 'foo';
 textRecognitionSettings.characterWhitelist = 'abc';
 
-const symbologySettings = new SymbologySettings();
+const symbologySettings = new Scandit.SymbologySettings();
 symbologySettings.enabled = true;
 symbologySettings.colorInvertedEnabled = false;
-symbologySettings.extensions = [SymbologySettings.Extension.TINY];
-symbologySettings.checksums = [SymbologySettings.Checksum.MOD_10];
+symbologySettings.extensions = [Scandit.SymbologySettings.Extension.TINY];
+symbologySettings.checksums = [Scandit.SymbologySettings.Checksum.MOD_10];
 symbologySettings.activeSymbolCounts = [1, 2];
 
-const barcodePicker = new BarcodePicker();
+const barcodePicker = new Scandit.BarcodePicker();
 barcodePicker.show(console.log, console.log, undefined, undefined, console.log);
 barcodePicker.show(callbacks);
 barcodePicker.cancel();
-barcodePicker.applyScanSettings(new ScanSettings());
+barcodePicker.applyScanSettings(new Scandit.ScanSettings());
 barcodePicker.setConstraints(constraints, constraints, 1);
-barcodePicker.setOrientations([BarcodePicker.Orientation.LANDSCAPE_LEFT]);
+barcodePicker.setOrientations([Scandit.BarcodePicker.Orientation.LANDSCAPE_LEFT]);
 barcodePicker.setMargins(margins, margins, 1);
 barcodePicker.pauseScanning();
 barcodePicker.resumeScanning();
@@ -79,14 +79,14 @@ barcodePicker.getOverlayView();
 barcodePicker.convertPointToPickerCoordinates(point);
 
 const overlay = barcodePicker.getOverlayView();
-overlay.setGuiStyle(ScanOverlay.GuiStyle.DEFAULT);
+overlay.setGuiStyle(Scandit.ScanOverlay.GuiStyle.DEFAULT);
 overlay.setBeepEnabled(false);
 overlay.setVibrateEnabled(false);
 overlay.setTorchEnabled(false);
 overlay.setTorchButtonMarginsAndSize(1, 1, 1, 1);
 overlay.setTorchButtonOffAccessibility('foo', 'foo');
 overlay.setTorchButtonOnAccessibility('foo', 'foo');
-overlay.setCameraSwitchVisibility(ScanOverlay.CameraSwitchVisibility.ALWAYS);
+overlay.setCameraSwitchVisibility(Scandit.ScanOverlay.CameraSwitchVisibility.ALWAYS);
 overlay.setCameraSwitchButtonMarginsAndSize(1, 1, 1, 1);
 overlay.setCameraSwitchButtonBackAccessibility('foo', 'foo');
 overlay.setCameraSwitchButtonFrontAccessibility('foo', 'foo');
@@ -104,31 +104,31 @@ overlay.setProperty('foo', 'bar');
 overlay.setTextRecognitionSwitchVisible(false);
 overlay.setMissingCameraPermissionInfoText('foo');
 overlay.updateOverlayIfExists();
-overlay.setMatrixScanHighlightingColor(ScanOverlay.MatrixScanState.LOCALIZED, 'foo');
+overlay.setMatrixScanHighlightingColor(Scandit.ScanOverlay.MatrixScanState.LOCALIZED, 'foo');
 
-const settings = new ScanSettings();
-settings.getSymbologySettings(Barcode.Symbology.CODE39);
-settings.setSymbologyEnabled(Barcode.Symbology.CODE39, true);
+const settings = new Scandit.ScanSettings();
+settings.getSymbologySettings(Scandit.Barcode.Symbology.CODE39);
+settings.setSymbologyEnabled(Scandit.Barcode.Symbology.CODE39, true);
 settings.setProperty('foo', 'bar');
-settings.recognitionMode = ScanSettings.RecognitionMode.CODE;
+settings.recognitionMode = Scandit.ScanSettings.RecognitionMode.CODE;
 settings.highDensityModeEnabled = false;
-settings.activeScanningAreaPortrait = new Rect(1, 1, 1, 1, );
-settings.activeScanningAreaLandscape = new Rect(1, 1, 1, 1, );
+settings.activeScanningAreaPortrait = new Scandit.Rect(1, 1, 1, 1, );
+settings.activeScanningAreaLandscape = new Scandit.Rect(1, 1, 1, 1, );
 settings.deviceName = 'foo';
 settings.matrixScanEnabled = true;
-settings.cameraFacingPreference = ScanSettings.CameraFacing.FRONT;
-settings.scanningHotSpot = new Point(1, 1);
+settings.cameraFacingPreference = Scandit.ScanSettings.CameraFacing.FRONT;
+settings.scanningHotSpot = new Scandit.Point(1, 1);
 settings.relativeZoom = 1;
 settings.maxNumberOfCodesPerFrame = 5;
 settings.codeRejectionEnabled = true;
-settings.textRecognition = new TextRecognitionSettings();
-settings.workingRange = ScanSettings.WorkingRange.STANDARD;
+settings.textRecognition = new Scandit.TextRecognitionSettings();
+settings.workingRange = Scandit.ScanSettings.WorkingRange.STANDARD;
 settings.codeCachingDuration = 1;
 settings.codeDuplicateFilter = 1;
 
-const scanCase = ScanCase.acquire(settings, { didScan: console.log });
+const scanCase = Scandit.ScanCase.acquire(settings, { didScan: console.log });
 scanCase.volumeButtonToScanEnabled(true);
 scanCase.scanBeepEnabled(true);
 scanCase.errorSoundEnabled(true);
-scanCase.setTimeout(20, ScanCase.State.ACTIVE, ScanCase.State.OFF);
+scanCase.setTimeout(20, Scandit.ScanCase.State.ACTIVE, Scandit.ScanCase.State.OFF);
 
